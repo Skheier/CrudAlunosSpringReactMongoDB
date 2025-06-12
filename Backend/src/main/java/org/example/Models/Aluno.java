cbd5529ab89f6dd6ba346861ce0e406e1b653dec
Mathias <mathiasandrademoura@hotmail.com>
2025-06-12 02:04:28 -0300

    newBegininining

---
+++ Backend/src/main/java/org/example/Models/Aluno.java
@@ -0,0 +1,22 @@
+package org.example.Models;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@ToString
+@Builder
+@Document
+@Getter @Setter
+public class Aluno {
+    @Id private String id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String endereco;
+
+}
+
