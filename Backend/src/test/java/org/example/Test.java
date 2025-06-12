2e75ed78d47a1aef00ec9e54f5f282019ceb2738
Mathias <mathiasandrademoura@hotmail.com>
2025-06-12 01:34:05 -0300

    addTest

---
+++ Backend/src/test/java/org/example/Test.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.example.Models.Aluno;
+import org.example.Services.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Test {
+
+    @Autowired private AlunoService alunoService;
+
+    @org.junit.jupiter.api.Test
+    public void mainTest(){
+
+
+        System.out.println();
+        Aluno alunum = Aluno.builder().id("2323").nome("Maicon").telefone("133314422").email("salvs@uniara.com").endereco("Rua tavares").build();
+
+        //act
+        Aluno savedAluno = alunoService.saveAluno(alunum);
+        System.out.println(savedAluno);
+
+        //assert
+        Assertions.assertThat(savedAluno).isNotNull();
+
+        System.out.println(alunoService.allAlunos());
+
+    }
+
+}
