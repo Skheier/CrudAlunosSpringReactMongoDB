adab2939fd3b81ce3a1c81e0d1b172c4f97fd09d
Mathias <mathiasandrademoura@hotmail.com>
2025-06-12 16:14:57 -0300

    addValidation.java

--- Backend/src/main/java/org/example/Models/Aluno.java
+++ Backend/src/main/java/org/example/Models/Aluno.java
@@ -1,5 +1,7 @@
 package org.example.Models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +17,8 @@ public class Aluno {
     @Id private String id;
     private String nome;
     private String telefone;
+    @Email
+    @NotBlank
     private String email;
     private String endereco;
 
